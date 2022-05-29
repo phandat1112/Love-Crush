64b7555a570596630d5ac87332f90c5c07f29977
Tuan Thai <tuantm61@fpt.edu.vn>
2022-05-29 13:34:57 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Nhớ ba không?",
+    introTitle: 'Nguyễn Thị Trúc Ngân (Cục Muối)',
+    introDesc: `Nếu phải dùng một con số để diễn tả mức độ anh nhớ em,
+    thì chắc anh phải viết ra thêm một phần mềm mới đề đo mức độ a nhớ em
+    rồi giải thích cho em hiểu.`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Thương ba không bé ơi ',
+    btnYes: 'Thương ba nhiều lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho cục vàng <3',
+    reply: 'Nhớ ba nhiều lắm ă. Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Ba biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'Thứ 3 ba về chở bé đi ăn nhen.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
